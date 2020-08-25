6972de99105c42ab76117fe01c1b53b4f4a59007
lucas.pereira <lusanp4@gmail.com>
2020-08-25 15:26:35 -0300

    finishing challenge

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,22 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  Column,
+} from 'typeorm';
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @CreateDateColumn()
   updated_at: Date;
 }
 
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,18 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const deleteResult = await transactionRepository.delete(id);
+    if (!deleteResult.affected) {
+      throw new AppError('Não foi possível deletar a transação, ID inválido');
+    }
   }
 }
 
